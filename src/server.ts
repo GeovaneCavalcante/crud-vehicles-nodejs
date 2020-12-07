4a44b694e0cbf25be22c1049cc084d3f7072ce9a
Geovane Feitosa Cavalcante <geovanefeitosacavalcante@gmail.com>
2020-12-06 23:50:27 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,15 @@ import 'reflect-metadata';
 
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 import * as swaggerDocument from './config/swagger.json';
+
 import routes from './routes';
 
 import './database';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(routes);
